0bd6fc1c8b61337475d583ebe5c3235d071f3c9e
Miguel Lock <miguellock333@gmail.com>
2025-05-02 15:50:38 -0400

    medium working changing icon disco mode

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -4,17 +4,17 @@ import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import CelebrationIcon from "@mui/icons-material/Celebration";
+import SickIcon from "@mui/icons-material/Sick";
 // import Link from "next/link";
 import { Link } from "@mui/material";
 import { useTheme } from "@/context/ThemeContext";
 
 export function AppFooter() {
-  const { themeMode, toggleMode } = useTheme();
+  const { themeMode, toggleMode, discoParty } = useTheme();
 
   const toggleDisco = () => {
     if (typeof window !== "undefined") {
-      const currentDisco = localStorage.getItem("disco_party") === "true";
-      localStorage.setItem("disco_party", String(!currentDisco));
+      localStorage.setItem("disco_party", String(!discoParty));
       window.location.reload();
     }
   };
@@ -34,7 +34,7 @@ export function AppFooter() {
         </Typography>
 
         <IconButton color="inherit" onClick={() => toggleDisco()}>
-          <CelebrationIcon />
+          {discoParty ? <SickIcon /> : <CelebrationIcon />}
         </IconButton>
 
         <IconButton color="inherit" onClick={toggleMode}>
